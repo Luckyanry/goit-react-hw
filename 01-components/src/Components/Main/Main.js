4385a37be5fbb8c2fe94f833744e1b421c7aa542
Andrii Kochmaruk <luckyanry@gmail.com>
2020-07-12 01:31:35 +0200

    fourth task of first react-hw is ready, but without styles

--- 01-components/src/Components/Main/Main.js
+++ 01-components/src/Components/Main/Main.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { Background } from "./Main.styled";
+
 import Profile from "../Profile/Profile";
 import Statistics from "../Statistics/Statistics";
 import FriendList from "../FriendList/FriendList";
+import TransactionHistory from "../TransactionHistory/TransactionHistory";
+
 import user from "../../bd/user.json";
 import statisticalData from "../../bd/statistical-data.json";
 import friends from "../../bd/friends.json";
+import transactions from "../../bd/transactions.json";
 
 const Main = () => {
   return (
@@ -18,7 +22,8 @@ const Main = () => {
         stats={user.stats}
       />
       <Statistics title="Upload stats" stats={statisticalData} />
-      <FriendList friends={friends} />,
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </Background>
   );
 };
